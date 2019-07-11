83b794f7a7fdf9ce954bc4277d956d181e61a28d
lusen <huancr@outlook.com>
2019-07-11 20:23:16 +0800

    草地交互测试

--- Assets/Scripts/VectorRenderObject.cs
+++ Assets/Scripts/VectorRenderObject.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class VectorRenderObject : MonoBehaviour
 {
     public Material material;
+	public bool autoAdd;
     public float size = 1.0f;
 
     private Mesh m_Mesh;
+
+	private List<Vector3> m_PositionList;
+
+	private Vector3 m_Position;
 
     void Start()
     {
@@ -22,7 +28,25 @@ public class VectorRenderObject : MonoBehaviour
             new Vector2(1, 1), new Vector2(1, 0),
         };
         m_Mesh.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+
+	    m_PositionList = new List<Vector3>();
+
+	    m_Position = transform.position;
+
     }
+
+	void Update()
+	{
+		if (autoAdd)
+		{
+			float dis = Vector3.Distance(transform.position, m_Position);
+			if (dis > 0.2f)
+			{
+				m_Position = transform.position;
+				m_PositionList.Add(transform.position);
+			}
+		}
+	}
 
     void OnDestroy()
     {
@@ -36,6 +60,15 @@ public class VectorRenderObject : MonoBehaviour
         {
             Matrix4x4 matrix = Matrix4x4.TRS(transform.position, Quaternion.identity, Vector3.one * size);
             VectorRenderer.DrawMesh(m_Mesh, material, matrix);
+
+	        if (autoAdd)
+	        {
+		        for (int i = 0; i < m_PositionList.Count; i++)
+		        {
+			        matrix = Matrix4x4.TRS(m_PositionList[i], Quaternion.identity, Vector3.one * size);
+			        VectorRenderer.DrawMesh(m_Mesh, material, matrix);
+				}
+	        }
         }
     }
 }
---
+++ Assets/Shaders/VectorRenderTest2.shader
@@ -0,0 +1,56 @@
+﻿Shader "Unlit/VectorRenderTest2"
+{
+    Properties
+    {
+		_Range ("Range", vector) = (1,1,1,1)
+    }
+    SubShader
+    {
+        Pass
+        {
+			blend srcalpha oneminussrcalpha
+			zwrite off
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+			float4 _Range;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				float2 dir = i.uv - float2(0.5,0.5);
+
+				float l = length(dir);
+
+				float c = 1.0-saturate((l - _Range.y) * _Range.z);
+
+				dir = lerp(float2(0, 0), dir, c) * 0.5 + 0.5;
+
+                return fixed4(dir.xy, 0, c);
+            }
+            ENDCG
+        }
+    }
+}
